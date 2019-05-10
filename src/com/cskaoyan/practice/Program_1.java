014d7fc0fa71f8618b47e5969caeaafce7ebb764
脑残2014 <zsq@live.cn>
2019-05-11 00:27:13 +0800

    哈哈

--- src/com/cskaoyan/practice/Program_1.java
+++ src/com/cskaoyan/practice/Program_1.java
@@ -8,7 +8,7 @@ public class Program_1 {
 	  
 	public static void main(String[] args) {
 		int a = 1, b =1;
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 5; i++) {
 			a = a + b;
 			b = b + a;
 			System.out.println(a + "\t" + b);
